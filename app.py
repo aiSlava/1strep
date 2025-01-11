6a1eff4d2f2309f2d201e5ecf797ebbf15982489
Viacheslav Shestakovskyi <slavashestakovskyi@MacBook-Air-3.local>
2025-01-11 08:55:26 +0200

    Fixed unterminated string literal in app.run

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def webhook():
     return jsonify({"status": "success", "make_response": response.text}), response.status_code
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0, port=5000)
+    app.run(host='0.0.0.0', port=5000)
